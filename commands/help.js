d74b69d8d813617671591a94a4fc9231f4e94583
Hippo137 <60151839+Hippo137@users.noreply.github.com>
2021-07-06 00:02:28 +0200

    .

--- commands/help.js
+++ commands/help.js
@@ -10,26 +10,8 @@ module.exports =
         dc = Discord;
         msg = message;
         
-        const embedMessage = new dc.MessageEmbed()
-            .setColor('#00c800')
-            .setTitle(`“${msg.content}”`)
-            .setDescription(`used by ${msg.author}`)
-            .addField('Tournament', 'Open', false)
-            .addField('Qualifiers', 'oq', true)
-            .addField('Day Final', 'od', true)
-            .addField('Semifinal', 'os', true)
-            .addField('Finals', 'of', true)
-            .addField('Tournament', 'Cash', false)
-            .addField('Qualifiers', 'cq', true)
-            //.addField('Day Final', 'cd', true)
-            .addField('Semifinal', 'cs', true)
-            .addField('Finals', 'cf', true)
-            .addField('Tournament', 'Open Cities & Knights', false)
-            .addField('Qualifiers', 'oqck', true)
-            //.addField('Day Final', 'odck', true)
-            .addField('Semifinal', 'osck', true)
-            .addField('Finals', 'ofck', true)
+        if (msg.guild.me.hasPermission('MANAGE_MESSAGES')) msg.delete();
         
-        msg.channel.send(embedMessage);
+        msg.channel.send(fs.readFileSync(`txt/${this.name}.txt`, 'utf8'));
     }
 };
