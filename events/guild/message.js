df00de45ebb2d945d95dfb1edb4ccfbcc49d966e
Hippo137 <60151839+Hippo137@users.noreply.github.com>
2021-07-09 00:09:13 +0200

    .

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,17 +1,14 @@
-var myID = 383011975057113088
-var botID = 848599387403059200
-
 module.exports = (Discord, client, message) =>
 {
-    if (message.author.id != botID && message.author.id != myID) //not written by the bot or myself
+    if (message.author.id != 848599387403059200 && message.author.id != 383011975057113088) //not written by the bot or myself
     {
         if (message.guild == null) //DM
         {
-            client.users.cache.get(myID).send(`${message.author} via DM:\n${message.cleanContent}`);
+            client.users.cache.get('383011975057113088').send(`${message.author} via DM:\n${message.cleanContent}`);
         }
-        else if (message.mentions.has(client.user) && !message.mentions.has(client.users.cache.get(myID))) //mentioned the bot on a server and not myself
+        else if (message.mentions.has(client.user) && !message.mentions.has(client.users.cache.get('383011975057113088'))) //mentioned the bot on a server and not myself
         {
-            client.users.cache.get(myID).send(`${message.author}:\n\n${message.cleanContent}\n\n${message.url}`);
+            client.users.cache.get('383011975057113088').send(`${message.author}:\n\n${message.cleanContent}\n\n${message.url}`);
         }
     }
     
@@ -21,7 +18,7 @@ module.exports = (Discord, client, message) =>
     }
     
     const prefix = process.env.PREFIX;
-    if (!message.content.startsWith(prefix) || message.author.id != myID && !message.member.roles.cache.some(role => role.name === 'Host')) return; //only I and Hosts can use commands
+    if (!message.content.startsWith(prefix) || message.author.id != '383011975057113088' && !message.member.roles.cache.some(role => role.name === 'Host')) return; //only I and Hosts can use commands
     
     const args = message.content.slice(prefix.length).split(/ +/); //removes the prefix and create a string array with each element separated by a space in the message
     const cmd = args.shift().toLowerCase(); //cmd is the first element in the list and is removed from the message. It is also changed to lowercase
