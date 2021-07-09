203240403b62108326a090a7536552ff022acf46
Hippo137 <60151839+Hippo137@users.noreply.github.com>
2021-07-09 02:13:43 +0200

    .

--- events/guild/message.js
+++ events/guild/message.js
@@ -2,7 +2,7 @@ module.exports = (Discord, client, message) =>
 {
     if (message.author != client.user)// && message.author.id != 383011975057113088) //not written by the bot or myself
     {
-        console.log(`1:${message.author}: ${message.mentions.has(client.user)}, ${message.mentions.has('383011975057113088')}, ${message.mentions.has('862840978654363669')}`);
+        console.log(`1:${message.author}: ${message.mentions.has(client.user)}, ${message.mentions.has('383011975057113088')}, ${message.mentions.has('848653342342774785')}`);
         
         
         if (message.guild == null) //DM
