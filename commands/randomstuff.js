2550840df3761d2e891630585139ba1158ac95c6
Hippo137 <60151839+Hippo137@users.noreply.github.com>
2021-07-09 22:03:07 +0200

    .

--- commands/randomstuff.js
+++ commands/randomstuff.js
@@ -5,7 +5,7 @@ module.exports = {
         //const channelTarget = client.channels.cache.find(channel => channel.name == 'inner-circle');
         const channelTarget = client.channels.cache.find(channel => channel.name == 'arschlöscher');
         //channelTarget.send('Ready for the table spam. I’ll just wait in case you change your mind again <@410606606183825428>');
-        channelTarget.send(<a:blob:805610421615853619>);
+        channelTarget.send('<a:blob:805610421615853619>');
         
         
     }
