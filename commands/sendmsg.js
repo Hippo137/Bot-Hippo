68b41ed488666ad8d635220cc20bb3f11fa774b1
Hippo137 <60151839+Hippo137@users.noreply.github.com>
2021-07-09 22:28:58 +0200

    .

--- commands/sendmsg.js
+++ commands/sendmsg.js
@@ -7,7 +7,7 @@ module.exports = {
         //channelTarget.send('Ready for the table spam. I’ll just wait in case you change your mind again <@410606606183825428>');
         //channelTarget.send('<a:blob:805610421615853619> <@278111514123304961>');
         let argString = ''
-        args.forEach(a => argString += a)
+        args.forEach(a => argString += `${a} `)
         channelTarget.send(argString);
         
     }
