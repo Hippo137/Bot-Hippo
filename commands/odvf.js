7a08f5ce4c1f4a0f123768b6801bf0772d00c6f8
Hippo137 <60151839+Hippo137@users.noreply.github.com>
2021-07-09 23:16:06 +0200

    .

---
+++ commands/odvf.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+var dc
+var msg
+
+module.exports =
+{
+    name: 'od',
+    execute(client, message, args, Discord)
+    {
+        dc = Discord;
+        msg = message;
+        
+        if (!args[0]) return errorMessage();
+        
+        const tournament = parseInt(args[0]);
+        
+        if (isNaN(tournament)) return errorMessage();
+        
+        if (tournament<1) return errorMessage();
+        
+        if (msg.guild.me.hasPermission('MANAGE_MESSAGES')) msg.delete();
+        
+        let botMessage = fs.readFileSync(`txt/${this.name}.txt`, 'utf8').replace(/{tournament}/g, tournament).replace(/{screenshots}/g, msg.guild.channels.cache.get('750490997045198880')); //msg.guild.channels.cache.get('849784629061746708'));
+        
+        const channelTarget = msg.guild.channels.cache.find(channel => channel.name === 'table-1');
+        channelTarget.send(botMessage);
+        
+        //message.channel.send(`${message.author} used the table spam command\nOpen Tournament Semifinals: ${tournament}`);
+        
+        const embedMessage = new dc.MessageEmbed()
+            .setColor('#00c800')
+            .setTitle(`“${msg.content}”`)
+            .setDescription(`used by ${msg.author}`)
+            .addField('tournament', tournament, false)
+        
+        msg.channel.send(embedMessage);
+        
+        setPresence(client, 3);
+    }
+};
+
+function errorMessage()
+{
+    const embedMessage = new dc.MessageEmbed()
+        .setColor('#ff0000')
+        .setTitle(`Error: “${msg.content}”`)
+        .addField('Required Syntax', `od tournament`, false)
+        .addField('tournament', '>0', true)
+
+    msg.channel.send(embedMessage);
+}
+
+async function setPresence(client, newPresence)
+{
+    await client.channels.cache.get('862422544652828713').messages.fetch('862425269063254069').then(message => message.edit(newPresence));
+}
---
+++ commands/ofvf.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+var dc
+var msg
+
+module.exports =
+{
+    name: 'of',
+    execute(client, message, args, Discord)
+    {
+        dc = Discord;
+        msg = message;
+        
+        if (!args[0]) return errorMessage();
+        
+        const tournament = parseInt(args[0]);
+        
+        if (isNaN(tournament)) return errorMessage();
+        
+        if (tournament<1) return errorMessage();
+        
+        if (msg.guild.me.hasPermission('MANAGE_MESSAGES')) msg.delete();
+        
+        let botMessage = fs.readFileSync(`txt/${this.name}.txt`, 'utf8').replace(/{tournament}/g, tournament).replace(/{screenshots}/g, msg.guild.channels.cache.get('750490997045198880')); //msg.guild.channels.cache.get('849784629061746708'));
+        for (let i=1; i<=16; i++)
+        {
+            let finalName = '';
+            let finalNameShort = '';
+            if (i<5)
+            {
+                finalNameShort = 'F';
+                finalName = 'Final';
+            }
+            else
+            {
+                finalNameShort = 'L';
+                finalName = 'Loser Final';
+            }
+            
+            const number = i<5 ? i : i-4;
+            const table = i<5 ? i : i+12;
+            const topX = i<5 ? i-1 : Math.floor((i-5)/3);
+            const bracket = /*i==1 ? 'Grand' :*/ `${16*topX+1}–${16*(topX+1)}`;
+            
+            const channelTarget = msg.guild.channels.cache.find(channel => channel.name == `table-`+table);
+            channelTarget.send(botMessage.replace(/{bracket}/g, bracket).replace(/{number}/g, number<10?'0'+number:number).replace(/{table}/g, table<10?'0'+table:table).replace(/{finalName}/g, finalName).replace(/{finalNameShort}/g, finalNameShort));
+        }
+
+        //message.channel.send(`${message.author} used the table spam command\nOpen Tournament Finals: ${tournament}`);
+        
+        const embedMessage = new dc.MessageEmbed()
+            .setColor('#00c800')
+            .setTitle(`“${msg.content}”`)
+            .setDescription(`used by ${msg.author}`)
+            .addField('tournament', tournament, false)
+        
+        msg.channel.send(embedMessage);
+        
+        setPresence(client, 3);
+    }
+};
+
+function errorMessage()
+{
+    const embedMessage = new dc.MessageEmbed()
+        .setColor('#ff0000')
+        .setTitle(`Error: “${msg.content}”`)
+        .addField('Required Syntax', `of tournament`, false)
+        .addField('tournament', '>0', true)
+
+    msg.channel.send(embedMessage);
+}
+
+async function setPresence(client, newPresence)
+{
+    await client.channels.cache.get('862422544652828713').messages.fetch('862425269063254069').then(message => message.edit(newPresence));
+}
