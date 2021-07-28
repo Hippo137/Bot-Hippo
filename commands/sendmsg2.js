6a6cf3fead01e03280615281ff70e4ce7357d90c
Hippo137 <60151839+Hippo137@users.noreply.github.com>
2021-07-28 21:08:48 +0200

    .

--- commands/sendmsg2.js
+++ commands/sendmsg2.js
@@ -3,7 +3,7 @@ module.exports = {
     execute(client, message, args)
     {
         //const channelTarget = client.channels.cache.find(channel => channel.name == 'inner-circle');
-        const channelTarget = client.channels.cache.find(channel => channel.name == 'arschlöscher');
+        const channelTarget = client.channels.cache.find(channel => channel.id === '750170377224978433');
         //channelTarget.send('Ready for the table spam. I’ll just wait in case you change your mind again <@410606606183825428>');
         //channelTarget.send('<a:blob:805610421615853619> <@278111514123304961>');
         let argString = ''
