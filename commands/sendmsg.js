4d3ab677b57e8c3ba10122f2497438b1feee2d5c
Hippo137 <60151839+Hippo137@users.noreply.github.com>
2021-07-28 21:38:13 +0200

    .

--- commands/sendmsg.js
+++ commands/sendmsg.js
@@ -2,8 +2,11 @@ module.exports = {
     name: 'sendmsg',
     execute(client, message, args)
     {
-        const channelTarget = client.channels.cache.find(channel => channel.name == 'inner-circle');
-        //const channelTarget = client.channels.cache.find(channel => channel.name == 'arschlöscher');
+        if (message.author.id !== '383011975057113088') message.reply('You need to be a :hippopotamus: to use this command :smiling_face_with_tear:');
+        
+        const channelTarget = client.channels.cache.find(channel => channel.id === '787152755420299265');
+        
+        //const channelTarget = client.channels.cache.find(channel => channel.name == 'inner-circle');
         //channelTarget.send('Ready for the table spam. I’ll just wait in case you change your mind again <@410606606183825428>');
         //channelTarget.send('<a:blob:805610421615853619> <@278111514123304961>');
         let argString = ''
--- commands/sendmsg2.js
+++ commands/sendmsg2.js
@@ -2,8 +2,12 @@ module.exports = {
     name: 'sendmsg2',
     execute(client, message, args)
     {
-        //const channelTarget = client.channels.cache.find(channel => channel.name == 'inner-circle');
+        if (message.author.id !== '383011975057113088') message.reply('You need to be a :hippopotamus: to use this command :smiling_face_with_tear:');
+        
         const channelTarget = client.channels.cache.find(channel => channel.id === '750170377224978433');
+        
+        //const channelTarget = client.channels.cache.find(channel => channel.name == 'inner-circle');
+        
         //channelTarget.send('Ready for the table spam. I’ll just wait in case you change your mind again <@410606606183825428>');
         //channelTarget.send('<a:blob:805610421615853619> <@278111514123304961>');
         let argString = ''
