611cc0033539b77cfab7a6c6e5c83412635c7abb
Hippo137 <60151839+Hippo137@users.noreply.github.com>
2021-09-06 21:42:59 +0200

    .

--- events/guild/message.js
+++ events/guild/message.js
@@ -4,11 +4,13 @@ module.exports = (Discord, client, message) =>
     {
         if (message.guild == null) //DM
         {
-            client.users.cache.get('383011975057113088').send(`${message.author} via DM:\n${message.content}`);
+            //client.users.cache.get('383011975057113088').send(`${message.author} via DM:\n${message.content}`);
+            client.users.fetch('383011975057113088').send(`${message.author} via DM:\n${message.content}`);
         }
         else if ((message.mentions.has(client.user) || message.mentions.has('848653342342774785') || message.mentions.has('849809423178268704')) && !(message.mentions.has('383011975057113088') || message.mentions.has('848612682781163622') || message.mentions.has('861750799821832223') || message.mentions.has('827244724091748363') || message.mentions.has('747216338833113149') || message.mentions.has('747217237034926081'))) //mentioned the bot on a server and not myself
         {
-            client.users.cache.get('383011975057113088').send(`${message.author}:\n\n${message.content}\n\n${message.url}`);
+            //client.users.cache.get('383011975057113088').send(`${message.author}:\n\n${message.content}\n\n${message.url}`);
+            client.users.fetch('383011975057113088').send(`${message.author}:\n\n${message.content}\n\n${message.url}`);
         }
         
         if (message.author.id == 884517731918032907) console.log(message.mentions.has(client.user) + message.mentions.has('848653342342774785') + message.mentions.has('849809423178268704') + message.mentions.has('383011975057113088') + message.mentions.has('848612682781163622') + message.mentions.has('861750799821832223') + message.mentions.has('827244724091748363') + message.mentions.has('747216338833113149') + message.mentions.has('747217237034926081'));
