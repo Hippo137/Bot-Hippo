57d8e3cff14b465cfb3fc227e00fa159c48efa8c
Hippo137 <60151839+Hippo137@users.noreply.github.com>
2022-01-07 19:29:06 +0100

    .

---
+++ commands/role.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('role')
+        .setDescription('Receive a list of members of a given role ')
+        .addRoleOption
+        (option =>
+            option.setName('role')
+            .setDescription('Select a role')
+            .setRequired(true)
+        ),
+
+    async execute(interaction)
+    {
+        await interaction.deferReply();
+        if (!interaction.member.roles.cache.find(role => role.name === 'CC Team')) return await interaction.editReply('You are not allowed to use this command.').catch(console.error);
+        
+        const roleToSearch = interaction.options.getRole('role');
+        
+        let messageToWrite = `Members with the role: ${roleToSearch.name}\n`;
+
+        await interaction.guild.members.fetch();
+        await interaction.guild.fetch().then(g => g.roles.fetch().then(r => r.find(role => role.id === roleToSearch.id).members.forEach(member => messageToWrite += `\n${member.user.tag}`)))
+        await interaction.editReply(`${messageToWrite}`);
+    }
+};
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const {Client, Collection, Intents} = require('discord.js');
 const fs = require('fs');
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]});
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
