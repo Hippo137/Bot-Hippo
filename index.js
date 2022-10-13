19942c5ea04fc32f8fb353784e3d8e9b785ca6f8
Hippo137 <60151839+Hippo137@users.noreply.github.com>
2022-10-13 18:07:03 +0200

    .

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ client.once('ready', () => {
       console.log(error);
     }*/
     
-    client.channels.cache.get('862422544652828713').messages.fetch(process.env.DATABASE).then(async dbMsg =>{
+    /*client.channels.cache.get('862422544652828713').messages.fetch(process.env.DATABASE).then(async dbMsg =>{
     dbMsg.edit(`Database
 
 
@@ -67,7 +67,7 @@ sRound: 1
 sRounds: 3
 sVp: 10
 zVp: +
-`).catch(console.error)})
+`).catch(console.error)})*/
 
 
     });
