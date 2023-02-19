a54a6129a40ce79a6abe12ad5b770571cea61cbb
Hippo137 <60151839+Hippo137@users.noreply.github.com>
2023-02-20 00:43:15 +0100

    .

---
+++ commands/wacky.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const g = require('../general.js');
+let success = false;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('wacky')
+		.setDescription('Returns a set of rules to spice up your game')
+        .addStringOption
+        (option =>
+            option.setName('mode')
+            .setDescription(`Desired game mode for the game`)
+            .setRequired(true)
+            .addChoice('Base', 'Base')
+            .addChoice('Cities & Knights', 'Cities & Knights')
+        ),
+    
+    async execute(interaction)
+    {
+        await interaction.deferReply();
+        success = false;
+        await command(interaction);
+        g.log(interaction, success);
+	}
+}
+    
+function command(interaction)
+{
+    let sMode = interaction.options.getString('mode');
+    let mode = '';
+    switch (sMode)
+    {
+        case 'Base': mode = 'Base'; break;
+        case 'Cities & Knight': mode = 'CK'; break;
+    }
+    let message = '';
+    do
+    {
+        let random = 1+Math.floor(Math.random()*10); //change this 10 when adding more
+        let filename = `txt/wacky${random}.txt`;
+        if (!fs.existsSync(filename)) //if the file doesn’t exist, it means that there might be separate files for the different modes
+        {
+            filename = `txt/wacky${random}${mode}.txt`
+            if (!fs.existsSync(filename)) continue;
+        }
+        message = fs.readFileSync(filename, 'utf8')
+            .replace(/{sMode}/g, sMode)
+            .replace(/{zMode}/g, sMode=='Base'?'+':'-')
+            .replace(/{sVp}/g, sMode=='Base'?'10':'13');
+        break;
+    } while (true)
+    
+    interaction.editReply(message).catch(console.error);
+    
+    success = true;
+’t}
