bec909084841e8c9ae55026b8ca2ccf6a6dd911a
Hippo137 <markus-westphal@online.de>
2023-04-26 22:45:40 +0200

    .

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ client.once('ready', () => {
     
     client.channels.cache.get('960288981419962448').send(`Logged in as ${client.user.tag}`).catch(console.error);
     
-    
     //fs.rmSync(path.join(__dirname, './txt/helpRolecount.txt'), {force: true});
     //fs.rmSync(path.join(__dirname, './commands/rolecount.js'), {force: true});
     
