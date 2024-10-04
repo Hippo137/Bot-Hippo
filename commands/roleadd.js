9a8f04cd8930c2052518d616e81d232ee29d601a
Hippo137 <60151839+Hippo137@users.noreply.github.com>
2024-10-04 23:56:46 +0200

    roleadd adds all tournament roles at once now

--- commands/roleadd.js
+++ commands/roleadd.js
@@ -35,18 +35,15 @@ module.exports = {
                 .addChoice('CK', 'CK')
                 .addChoice('CKSF', 'CKSF')
             )
-            .addStringOption
+            .addIntegerOption
             (option =>
-                option.setName('event')
-                .setDescription('tournament event – required for non-Weekday, ignored for Weekday')
+                option.setName('events')
+                .setDescription('tournament events – required for non-Weekday, ignored for Weekday')
                 .setRequired(false)
-                .addChoice('Finals', 'Finals')
-                .addChoice('Qualifier (only 1 in total)', 'Qualifier')
-                .addChoice('Qualifier 1', 'Q1')
-                .addChoice('Qualifier 2', 'Q2')
-                .addChoice('Qualifier 3', 'Q3')
-                .addChoice('Qualifier 4', 'Q4')
-                .addChoice('Qualifier 5', 'Q5')
+                .addChoice('One Qualifier and Final', 1)
+                .addChoice('Two Qualifiers and Final', 2)
+                .addChoice('Three Qualifiers and Final', 3)
+                .addChoice('Four Qualifiers and Final', 4)
             )
         )
         .addSubcommand(subcommand => subcommand
@@ -129,22 +126,27 @@ async function command(interaction)
 {
     if (!g.allowed(interaction, 1)) return interaction.editReply('You are not allowed to use this command.').catch(console.error);
     
-    let name, position, hoist;
+    const nameSuffix = ['Finals', 'Q1', 'Q2', 'Q3', 'Q4'];
+    let position, hoist;
+    const number = interaction.options.getInteger('number');
+    const type = interaction.options.getString('type');
+    const mode = interaction.options.getString('mode');
+    const events = interaction.options.getInteger('events');
     switch (interaction.options.getSubcommand())
     {
         case 'week':
-        if (interaction.options.getString('type') != 'Weekday' && !interaction.options.getString('event'))
+        if (type != 'Weekday' && !events)
         {
-            return interaction.editReply('For non-Weekday tournaments, you must provide the event option.').catch(console.error);
+            return interaction.editReply('For non-Weekday tournaments, you must provide the events option.').catch(console.error);
         }
-        name = `${interaction.options.getInteger('number')} ${interaction.options.getString('type')} ${interaction.options.getString('mode')}`;
-        if (interaction.options.getString('type') != 'Weekday') name += ` ${interaction.options.getString('event')}`;
+        name = `${number} ${type} ${mode}`;
+        //if (interaction.options.getString('type') != 'Weekday') name += ` ${interaction.options.getString('event')}`;
         position = interaction.guild.roles.cache.find(role => role.name === 'Back Up Hero').position;
         hoist = true;
         break;
         
         case 'month':
-        name = `${interaction.options.getString('month')} ${interaction.options.getInteger('year')} ${interaction.options.getString('mode')}`;
+        name = `${interaction.options.getString('month')} ${interaction.options.getInteger('year')} ${mode}`;
         position = 1+interaction.options.getRole('rolebelow').position;
         hoist = false;
         break;
@@ -157,21 +159,30 @@ async function command(interaction)
     }
     
     success = true;
-    await interaction.guild.roles.create(
+    let i = 1, last = false;
+    do
     {
-        position: position,
-        hoist: hoist,
-        name: name,
-        color: 'RANDOM'
-    }).catch((e) =>
-    {
-        if (e.stack.startsWith('DiscordAPIError: Maximum number of server roles reached'))
+        await interaction.guild.roles.create(
         {
-            success = false; //return doesn’t work in await/catch. Without this, the reply would be edited again at the bottom overriding the error message from here
-            return interaction.editReply('Couldn’t create a new role. Maximum number of roles reached.').catch(console.error); //error handling in case the message was manually removed in the meantime
-        }
-    });
-    if (success) interaction.editReply(`Successfully created the new role ‘${name}’.`).catch(console.error); //error handling in case the message was manually removed
+            position: position,
+            hoist: hoist,
+            name: `${name} ${nameSuffix[i]}`,
+            color: 'RANDOM'
+        }).catch((e) =>
+        {
+            if (e.stack.startsWith('DiscordAPIError: Maximum number of server roles reached'))
+            {
+                success = false; //return doesn’t work in await/catch. Without this, the reply would be edited again at the bottom overriding the error message from here
+                return interaction.editReply('Couldn’t create all new roles. Maximum number of roles reached.').catch(console.error); //error handling in case the message was manually removed in the meantime
+            }
+        });
+        if (last == true) break;
+        position++;
+        if (++i > events) {last = true; i = 0};
+    }
+    while (true);
+    
+    if (success) interaction.editReply(`Successfully created the new roles for the ${type} ${mode} Tournament ${number}.`).catch(console.error); //error handling in case the message was manually removed
     
     //success = true;
 }
